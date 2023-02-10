5ccbd867838754483d205f4529b43ec9ff30b2ed
gsheppar <gsheppar@gmail.com>
2023-02-10 11:17:16 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# Prisma SD-WAN VPN Override Reset (Preview)
+The purpose of this script reset the VPN Override on a all sites for a specific network circuit to default.
+
+#### License
+MIT
+
+#### Requirements
+* Active CloudGenix Account - Please generate your API token and add it to cloudgenix_settings.py
+* Python >=3.7
+
+#### Installation:
+ Scripts directory. 
+ - **Github:** Download files to a local directory, manually run the scripts. 
+ - pip install -r requirements.txt
+
+### Examples of usage:
+ Please generate your API token and add it to cloudgenix_settings.py
+
+ 1. ./vpn_remove_override.py --name "Comcast"
+      - Will reset the VPN Override to default for circuits attached WAN Comcast
+
+### Caveats and known issues:
+ - This is a PREVIEW release, hiccups to be expected. Please file issues on Github for any problems.
+
+#### Version
+| Version | Build | Changes |
+| ------- | ----- | ------- |
+| **1.0.0** | **b1** | Initial Release. |
+
+
+#### For more info
+ * Get help and additional Prisma SD-WAN Documentation at <https://docs.paloaltonetworks.com/prisma/cloudgenix-sd-wan.html>
---
+++ vpn_remove_override.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+import cloudgenix
+import argparse
+from cloudgenix import jd, jd_detailed, jdout
+import yaml
+import cloudgenix_settings
+import sys
+import logging
+import ipcalc
+import ipaddress
+import os
+import datetime
+import sys
+import csv
+
+
+# Global Vars
+TIME_BETWEEN_API_UPDATES = 60       # seconds
+REFRESH_LOGIN_TOKEN_INTERVAL = 7    # hours
+SDK_VERSION = cloudgenix.version
+SCRIPT_NAME = 'CloudGenix: WAN Interface VPN Override Reset'
+SCRIPT_VERSION = "1"
+
+# Set NON-SYSLOG logging to use function name
+logger = logging.getLogger(__name__)
+
+####################################################################
+# Read cloudgenix_settings file for auth token or username/password
+####################################################################
+
+sys.path.append(os.getcwd())
+try:
+    from cloudgenix_settings import CLOUDGENIX_AUTH_TOKEN
+
+except ImportError:
+    # Get AUTH_TOKEN/X_AUTH_TOKEN from env variable, if it exists. X_AUTH_TOKEN takes priority.
+    if "X_AUTH_TOKEN" in os.environ:
+        CLOUDGENIX_AUTH_TOKEN = os.environ.get('X_AUTH_TOKEN')
+    elif "AUTH_TOKEN" in os.environ:
+        CLOUDGENIX_AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+    else:
+        # not set
+        CLOUDGENIX_AUTH_TOKEN = None
+
+def vpn_remove_override(cgx, wan_name):  
+    wannetwork_id = []
+    for wannetworks in cgx.get.wannetworks().cgx_content['items']:
+        if wannetworks["name"] == wan_name:
+            wannetwork_id = wannetworks["id"]
+    if not wannetwork_id:
+        print("Unable to find WAN Network " + wan_name)
+    
+    for site in cgx.get.sites().cgx_content['items']:
+        for waninterface in cgx.get.waninterfaces(site_id=site["id"]).cgx_content['items']:
+            if waninterface["network_id"] == wannetwork_id:
+                if waninterface["vpnlink_configuration"]:
+                    waninterface["vpnlink_configuration"] = None
+                    resp = cgx.put.waninterfaces(site_id=site["id"],waninterface_id=waninterface["id"], data = waninterface)
+                    if not resp:
+                        print("Error updating " + site["name"] + " wan interface " + waninterface["name"])
+                        print(str(jdout(resp)))
+                    else:
+                        print("Updating " + site["name"] + " wan interface " + waninterface["name"])
+                    
+    
+
+    return
+                                 
+def go():
+    ############################################################################
+    # Begin Script, parse arguments.
+    ############################################################################
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="{0}.".format(SCRIPT_NAME))
+
+    # Allow Controller modification and debug level sets.
+    controller_group = parser.add_argument_group('API', 'These options change how this program connects to the API.')
+    config_group = parser.add_argument_group('Name', 'These options change how the configuration is loaded.')
+    config_group.add_argument("--name", "-N", help="WAN Name", required=True, default=None)
+    controller_group.add_argument("--controller", "-C",
+                                  help="Controller URI, ex. "
+                                       "Alpha: https://api-alpha.elcapitan.cloudgenix.com"
+                                       "C-Prod: https://api.elcapitan.cloudgenix.com",
+                                  default=None)
+    controller_group.add_argument("--insecure", "-I", help="Disable SSL certificate and hostname verification",
+                                  dest='verify', action='store_false', default=True)
+    login_group = parser.add_argument_group('Login', 'These options allow skipping of interactive login')
+    login_group.add_argument("--email", "-E", help="Use this email as User Name instead of prompting",
+                             default=None)
+    login_group.add_argument("--pass", "-PW", help="Use this Password instead of prompting",
+                             default=None)
+    debug_group = parser.add_argument_group('Debug', 'These options enable debugging output')
+    debug_group.add_argument("--debug", "-D", help="Verbose Debug info, levels 0-2", type=int,
+                             default=0)
+                             
+    args = vars(parser.parse_args())
+    
+    ############################################################################
+    # Instantiate API
+    ############################################################################
+    cgx_session = cloudgenix.API(controller=args["controller"], ssl_verify=args["verify"])
+
+    # set debug
+    cgx_session.set_debug(args["debug"])
+
+    ##
+    # ##########################################################################
+    # Draw Interactive login banner, run interactive login including args above.
+    ############################################################################
+    print("{0} v{1} ({2})\n".format(SCRIPT_NAME, SCRIPT_VERSION, cgx_session.controller))
+
+    # login logic. Use cmdline if set, use AUTH_TOKEN next, finally user/pass from config file, then prompt.
+    # check for token
+    if CLOUDGENIX_AUTH_TOKEN and not args["email"] and not args["pass"]:
+        cgx_session.interactive.use_token(CLOUDGENIX_AUTH_TOKEN)
+        if cgx_session.tenant_id is None:
+            print("AUTH_TOKEN login failure, please check token.")
+            sys.exit()
+
+    else:
+        while cgx_session.tenant_id is None:
+            cgx_session.interactive.login(user_email, user_password)
+            # clear after one failed login, force relogin.
+            if not cgx_session.tenant_id:
+                user_email = None
+                user_password = None
+
+    ############################################################################
+    # End Login handling, begin script..
+    ############################################################################
+
+    # get time now.
+    curtime_str = datetime.datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
+
+    # create file-system friendly tenant str.
+    tenant_str = "".join(x for x in cgx_session.tenant_name if x.isalnum()).lower()
+    cgx = cgx_session
+    wan_name = args["name"]
+
+    vpn_remove_override(cgx, wan_name) 
+    # end of script, run logout to clear session.
+    print("End of script. Logout!")
+    cgx_session.get.logout()
+
+if __name__ == "__main__":
+    go()
